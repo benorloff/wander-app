5818d60020fcb5c0d61d2d0ed81d38b04fbabbe9
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-08 14:42:17 -0700

    add home view and header and footer views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
-  res.send('Index is working');
+  res.render('home');
 });
 
 // Google OAuth login route
