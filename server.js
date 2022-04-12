4a2b164b07b085af27c62140cc264a92d513e1cd
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-11 20:47:37 -0700

    add body-parser to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 // session middleware
 const session = require('express-session');
 const passport = require('passport');
@@ -32,6 +33,8 @@ app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
