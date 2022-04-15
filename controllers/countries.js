b3d49b2d9ca7556fd54afb3213a7a82c3efc698a
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-15 13:55:35 -0700

    fix updateBadge function bug

--- controllers/countries.js
+++ controllers/countries.js
@@ -33,7 +33,7 @@ async function addVisitor(req, res) {
     try {
         const country = await Country.findById(req.params.id);
         country.usersVisited.push(req.user._id);
-        country.save();
+        await country.save();
         const usersCountries = await Country.find({usersVisited: req.user._id}).exec();
         const usersCountryCount = await usersCountries.length;
         updateBadges(req, usersCountryCount);
@@ -46,32 +46,35 @@ async function addVisitor(req, res) {
 async function removeVisitor(req, res) {
     try {
         const country = await Country.findById(req.params.id);
+        console.log(`${req.user._id} <- user.id to remove`)
+        if (country.usersVisited.includes(req.user._id)) {
+            country.usersVisited.remove(req.user._id);
+            await country.save();
+        }
         const usersCountries = await Country.find({usersVisited: req.user._id}).exec();
         const usersCountryCount = await usersCountries.length;
-        const userIdx = await country.usersVisited.indexOf(req.user._id);
-        if (userIdx > -1) {
-            country.usersVisited.splice(userIdx, 1);
-            country.save();
-            updateBadges(req,usersCountryCount);
-            res.redirect('/countries/all');
-        }
+        updateBadges(req, usersCountryCount);
+        res.redirect('/countries/all');
     } catch (err) {
         res.send(err);
     }
 }
 
 async function updateBadges(req, usersCountryCount) {
+    console.log('hit updateBadges function');
+    console.log(usersCountryCount);
     const countryBadges = await Badge.find({name: {$regex: /^countries/}}).exec();
-    countryBadges.forEach(b => {
+    console.log(`${countryBadges} <- country badges`)
+    await countryBadges.forEach(b => {
         // Remove user from badge if their country count is below the badge's value
         if (usersCountryCount < b.numValue && b.usersCollected.includes(req.user._id) === true) {
-            let userIdx = b.usersCollected.indexOf(req.user._id);
-            b.usersCollected.splice(userIdx, 1);
-            b.save();
+            b.usersCollected.remove(req.user._id);
+            console.log('user REMOVED from badge')
         } else if (usersCountryCount >= b.numValue && b.usersCollected.includes(req.user._id) === false) {
             b.usersCollected.push(req.user._id);
-            b.save();
+            console.log('user ADDED to badge')
         }
+        b.save();
     })
 }
 
--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,6 @@ async function show(req, res) {
     const userCountries = await Country.find({usersVisited: req.user._id}).exec();
     const userPosts = await Post.find({user: req.user._id}).exec();
     const userBadges = await Badge.find({usersCollected: req.params.id}).exec();
-    console.log(userBadges);
     res.render('users/show', {title: 'User Profile', userCountries, userPosts, userBadges})
 };
 
